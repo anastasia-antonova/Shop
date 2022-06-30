f59c14a5d71c688763ffa402d3ac422a9973c8ca
anastasia-antonova <annastasia611@gmail.com>
2022-06-30 10:17:02 +0300

    add test js

---
+++ js/test.js
@@ -0,0 +1,119 @@
+let nick;
+console.log(nick)
+let age = 12;
+let numberOfSiblings = 1 + 3;
+let numberOfCandies = 8;
+console.log(numberOfCandies / numberOfSiblings);
+let secondsInAMinute = 60;
+let minutesInAHour = 60;
+let secondsInAHour = secondsInAMinute * minutesInAHour;
+console.log(secondsInAHour)
+let highFives = 0;
+++highFives;
+console.log(highFives)
+++highFives;
+++highFives;
+console.log(highFives);
+let x = 10;
+x = x + 5;
+console.log(x);
+x += 2;
+console.log(x);
+
+let greeting = "Hello";
+let myName = "Nick";
+console.log(greeting + myName);
+let java = "Java";
+console.log(java.length);
+console.log(myName[2]);
+console.log(myName[myName.length - 1]);
+let longString = "My long string is long";
+console.log(longString.slice(3, 14));
+console.log("Hello there, how are you doing?".toUpperCase());
+
+
+let hasApple = true;
+let hasOrange = false;
+console.log(hasApple || hasOrange);
+console.log(!hasApple);
+
+let isWeekend = false;
+let hadShower = true;
+ hasApple = false;
+hasOrange = true;
+let shouldGoToSchool = !isWeekend && hadShower && (hasApple || hasOrange)
+console.log(shouldGoToSchool)
+
+let height = 65;
+let heightRestriction = 60;
+console.log(height > heightRestriction);
+
+ height = 60;
+ heightRestriction = 60;
+console.log(height > heightRestriction);
+
+console.log(height >= heightRestriction);
+
+let mySecretNumber = 5;
+let chico = 3;
+console.log(mySecretNumber === chico);
+let harpo = 7;
+console.log(mySecretNumber === harpo)
+let groucho = 5;
+console.log(mySecretNumber === groucho);
+
+let stringNumber = "5";
+let actualNumber = 5;
+console.log(stringNumber === actualNumber);
+console.log(stringNumber == actualNumber);
+
+// масиви
+let dinosaurs = ["T-Rex", "Velociraptor", "Stegosaurus", "Triceratos", "Brachi", "Pteranoton"];
+console.log(dinosaurs[0])
+console.log(dinosaurs[3])
+
+let dinosaursAndNumber = [3, "dinosaur", ["triceraptor","stenozavr",364.5], 10]
+console.log(dinosaursAndNumber[2]);
+console.log(dinosaursAndNumber[2][0]);
+
+console.log(dinosaurs.length)
+console.log(dinosaurs[dinosaurs.length - 1])
+console.log(dinosaurs)
+
+console.log(dinosaurs.push("cat"))
+console.log(dinosaurs.push("dog"))
+console.log(dinosaurs.pop())
+console.log(dinosaurs)
+console.log(dinosaurs.pop())
+
+console.log(dinosaurs.concat(greeting))
+console.log(dinosaurs.concat(greeting, dinosaursAndNumber))
+console.log(dinosaursAndNumber)
+console.log(dinosaurs.indexOf("T-Rex"))
+
+let boringAnimals = ["Monkay", "Cat", "Fish",
+"Lizart"]
+console.log(boringAnimals.join())
+console.log(boringAnimals.join(","))
+console.log(boringAnimals.join("7"))
+console.log(boringAnimals.join(" ? "))
+console.log(boringAnimals.join(" "))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
